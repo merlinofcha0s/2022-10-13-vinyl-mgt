2e475146189551bc7ec488ca94729bbbf4b3d645
Cyril Casaucau <casaucau.cyril@protonmail.com>
2022-10-14 16:39:49 +0200

    Service hardcoded data

--- src/app/vinyl/list-vinyl/list-vinyl.component.ts
+++ src/app/vinyl/list-vinyl/list-vinyl.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Vinyl} from "../vinyl.model";
+import {VinylService} from "../vinyl.service";
 
 @Component({
   selector: 'app-list-vinyl',
@@ -8,40 +9,14 @@ import {Vinyl} from "../vinyl.model";
 })
 export class ListVinylComponent implements OnInit {
 
-  public vinyls: Array<Vinyl> = [
-    {
-      id: 0,
-      title: 'Bad',
-      artiste: 'Michael Jackson',
-      imageUrl: 'https://img-4.linternaute.com/PI-AYLwdrSAlAsBVE53jZasrWYM=/350x/smart/4184ae2fdda74cb5b2e150bc98efebe8/ccmcms-linternaute/19006.jpg',
-      releaseDate: new Date('1987-10-1'),
-    },
-    {
-      id: 1,
-      title: 'Uprising',
-      artiste: 'Bob marley and the Wailers',
-      imageUrl: 'https://img.cdandlp.com/2017/06/imgL/118865816.jpg',
-      releaseDate: new Date('1980-07-10'),
-    },
-    {
-      id: 2,
-      title: 'Hendrix in the west',
-      artiste: 'Jimi Hendrix',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Hendrix_in_the_west.jpg',
-      releaseDate: new Date('1972-02-1'),
-    },
-    {
-      id: 3,
-      title: 'Porgy and Bess',
-      artiste: 'Louis Armstrong et Ella Fitzgerald',
-      imageUrl: 'https://media1.jpc.de/image/w600/front/0/8436542011464.jpg',
-      releaseDate: new Date('1958-10-30'),
-    }
-  ];
+  public vinyls: Array<Vinyl> = [];
 
-  constructor() { }
+  constructor(private vinylService: VinylService) {
+    // Ne pas mettre de code ou d'appel de service dans le constructeur.
+  }
 
   ngOnInit(): void {
+   this.vinyls = this.vinylService.getAll();
   }
 
 }
